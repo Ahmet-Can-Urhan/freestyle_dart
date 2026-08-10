8576b7b840d7617630c07d29bc708cbaf5a5d83b
Ahmet Can Urhan <s4bre.py@gmail.com>
2026-08-11 01:05:07 +0300

    dart analyze fix (empty_statements)

--- lib/solutions/challenge_001.dart
+++ lib/solutions/challenge_001.dart
@@ -1,5 +1,5 @@
 List<int> getEvenNumbers(List<int> numbers) {
 
-  final solution = numbers.where((x) => x % 2 == 0).toList();;
+  final solution = numbers.where((x) => x % 2 == 0).toList();
   return solution;
 }
--- lib/solutions/challenge_003.dart
+++ lib/solutions/challenge_003.dart
@@ -2,6 +2,6 @@ int sumPositiveNumbers(List<int> numbers) {
   
   final solution =numbers
       .where((x) => x > 0)
-      .fold(0, (previousValue, element) => previousValue + element);;
+      .fold(0, (previousValue, element) => previousValue + element);
   return solution;
 }
