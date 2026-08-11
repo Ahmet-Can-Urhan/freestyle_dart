66e015d80e5a83d3d4dcf9e5165b785a4dba79be
Ahmet Can Urhan <s4bre.py@gmail.com>
2026-08-11 20:27:34 +0300

    challenge fix

--- lib/solutions/challenge_009.dart
+++ lib/solutions/challenge_009.dart
@@ -1,6 +1,7 @@
 Map<String, int> countWordOccurrences(List<String> words) {
-  return words.fold(<String, int>{}, (result, word) {
+  final result = words.fold(<String, int>{}, (result, word) {
     result[word] = (result[word] ?? 0) + 1;
     return result;
   });
+  return result;
 }
