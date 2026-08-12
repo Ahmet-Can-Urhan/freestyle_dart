ea62e035d7164493ef129f32b354e7b874771809
Ahmet Can Urhan <s4bre.py@gmail.com>
2026-08-12 04:33:04 +0300

    dart format

--- lib/solve_me/challenge_010.dart
+++ lib/solve_me/challenge_010.dart
@@ -1,10 +1,14 @@
-String findMostFrequentWord(List<String> words) 
-{
-  final result = words.fold(<String, int>{}, (result,word) 
-  {
-    result[word] = (result[word] ?? 0) + 1;
-    return result;
-  }).entries.reduce((value,element) => (value.value > element.value ? value : element)).key;
+String findMostFrequentWord(List<String> words) {
+  final result = words
+      .fold(<String, int>{}, (result, word) {
+        result[word] = (result[word] ?? 0) + 1;
+        return result;
+      })
+      .entries
+      .reduce(
+        (value, element) => (value.value > element.value ? value : element),
+      )
+      .key;
 
   return result;
 }
