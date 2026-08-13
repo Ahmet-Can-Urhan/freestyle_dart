bec497887d39401d41a6d1896d4edabe3e151c58
Ahmet Can Urhan <s4bre.py@gmail.com>
2026-08-13 06:50:42 +0300

    dart format

--- lib/solutions/challenge_014.dart
+++ lib/solutions/challenge_014.dart
@@ -1,20 +1,15 @@
-Map<String, List<Product>> groupProductsByCategory(List<Product> products) 
-{
-  final result = products.fold(<String,List<Product>>{}, (result, element) 
-  {
+Map<String, List<Product>> groupProductsByCategory(List<Product> products) {
+  final result = products.fold(<String, List<Product>>{}, (result, element) {
     result.putIfAbsent(element.category, () => []).add(element);
     return result;
   });
   return result;
 }
 
-
-
 //Do not modify this class.
-class Product 
-{
+class Product {
   final String name;
   final String category;
 
-  const Product({required this.name,required this.category});
+  const Product({required this.name, required this.category});
 }
--- lib/solve_me/challenge_014.dart
+++ lib/solve_me/challenge_014.dart
@@ -1,18 +1,14 @@
-Map<String, List<Product>> groupProductsByCategory(List<Product> products) 
-{
+Map<String, List<Product>> groupProductsByCategory(List<Product> products) {
   //TODO: Implement this function.
 
   final result = null;
   return result;
 }
 
-
-
 //Do not modify this class.
-class Product 
-{
+class Product {
   final String name;
   final String category;
 
-  const Product({required this.name,required this.category});
+  const Product({required this.name, required this.category});
 }
